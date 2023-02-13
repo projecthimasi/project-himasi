f5aeacc1fa8758d55b8d74004bf5fe21988c6342
Alvin Austin <68804883+wandesay75@users.noreply.github.com>
2023-02-13 21:24:49 +0700

    nyicil project

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,6 +5,49 @@ class Auth extends CI_Controller {
 
     public function index()
 	{
-		$this->load->view('auth/login');
+		$data['title'] = 'HIMASI BSI | Login';
+		$this->load->view('auth/login', $data);
 	}
+
+	public function login(){
+		$email = $this->db->get_where('mahasiswa', ['email' => $this->input->post('email', true)])->row();
+        if ($email){
+            if(password_verify($this->input->post('password'), $email->password)){
+                if ($email->status == 'admin'){
+                    $data_session = [
+                         'id' => $email->id,
+                         'nama' => $email->nama,
+                         'status' => $email->status,
+                     ];
+                    $this->session->set_userdata($data_session);
+                    redirect("admin/dashboard");
+                } else {
+                    $data_session = [
+                         'id' => $email->id,
+                         'nama' => $email->nama,
+                         'status' => $email->status,
+                    ];
+                    $this->session->set_userdata($data_session);
+                    redirect("landing_page");
+                }
+
+            }else { 
+                echo "<script>
+                alert('password anda salah!');
+                window.location.href = `" . site_url('auth/login') . "`;
+                </script>";
+               }
+
+        } else {
+            echo "<script>
+                alert('NIK anda tidak terdaftar!');
+                window.location.href = `" . site_url('auth/login') . "`;
+             </script>";
+        }
+	}
+
+	public function logout(){
+        $this->session->sess_destroy();
+        redirect('auth/login');
+    }
 }
